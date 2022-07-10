593dc18d17996c863b39e8c7a35bf1035b41b9a2
sui-sfc <sui@sfc.wide.ad.jp>
2022-07-11 01:39:41 +0900

    add stream

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 
 import socket
 import struct
+'''
+import pyaudio
+
+
+CHUNK =1024
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16,
+                channels=2,
+                rate=48000,
+                output=True,
+                frames_per_buffer=CHUNK
+                )
+
+'''
 MCAST_GRP = '239.69.249.134'
 MCAST_PORT = 5004
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(('', MCAST_PORT))
@@ -10,44 +25,15 @@ mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 n = 0
 
+
+
 RED='\033[31m'
 END='\033[0m'
 
 while True:
+    #sound, address = sock.recvfrom(CHUNK*4)
+    #stream.write(sound, CHUNK)
     print(RED+str(n)+END)
-    print(sock.recv(1024))
     n+=1
 
-'''
-from socket import socket, AF_INET, SOCK_DGRAM
-#import pyaudio
-import wave
-import sys
-
-CHUNK = 1024
-HOST = ''
-PORT = 319
-
-
-p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16,
-                channels=2,
-                rate=44100,
-                output=True,
-                frames_per_buffer=CHUNK
-                )
-
-s = socket(AF_INET, SOCK_DGRAM)
-s.bind((HOST, PORT))
-
-sound, address = s.recvfrom(CHUNK*4)
-
-print('nowplaying from ' + str(address))
-while sound != b'':
-    #stream.write(sound, CHUNK)
-    sound, address = s.recvfrom(CHUNK*4)
-
-print('stop')
-#stream.close()
-s.close()
-'''
+stream.close()
